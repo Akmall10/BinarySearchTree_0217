c3e9a90ebecf3508d47644f0ec3861f69a75f805
Akmall10 <akmalprasety53@gmail.com>
2025-06-24 10:35:38 +0700

    step4

--- binarysearchTree.cpp
+++ binarysearchTree.cpp
@@ -3,57 +3,71 @@ using namespace std;
 
 class Node
 {
-    public:
-      int info;
-      Node *leftchild;
-      Node *rightchild;
+public: 
+    int info;
+    Node *leftchild;
+    Node *rightchild;
 
-      // constructor for the node class
-      Node()
-      {
-        leftchild = nullptr; // initialized left child to null
-        rightchild = nullptr; // initialized rightchild to null
-
-      }
+  // constructor for the node class
+    Node()
+    {
+        leftchild = nullptr; // initialize left child to null
+        rightchild = nullptr; // initialize right child to null
+    }
 };
 
 class BinaryTree
 {
-   public:
-   Node *ROOT;
+public:
+    Node *ROOT;
 
-   BinaryTree()
-   {
-     ROOT = nullptr;// initialized ROOT to null
-   }
+    BinaryTree()
+    {
+        ROOT = nullptr; // initialize ROOT to null 
+    }
 
-   void insert()
-   {
-      int x;
-      cout << "Masukan nilai: ";
-      cin >> x;
+    void insert()
+    {
+        int x;
+        cout << "Masukkan nilai: ";
+        cin >> x; 
 
-      // step 1: Allocate memory for the new node
-      Node *newNode = new Node()
+        // step 1: Allocate memory for the new node
+        Node *newNode = new Node();
 
-      // step 2: Asssign value to the data field of new node 
-      newNode->info = x;
+        // step 2: Assign value to the data field of new node
+        newNode->info = x;
 
-      // step 3: Make the left and right child of the new node point to NULL
-      newNode->leftchild = nullptr;
-      newNode->rightchild= nullptr;
+        // step 3: make the left and right child of the new node point to NULL
+        newNode->leftchild = nullptr;
+        newNode->rightchild = nullptr;
 
-      //step 4: locate the node with will be the parrent pf the node to be inserted
-      Node *parent = nullptr;
-      Node *currentNode = nullptr;
-      search(x,parent, currentNode);
+        // step 4: locate the node which will be the parrent of the node to be inserted
+        Node *parent = nullptr;
+        Node *currentNode = nullptr;
+        search(x, parent, currentNode);
 
-      // step 5: if parent is NULL ( Tree is empty)
-      if (parent = nullptr)
-      {
-        // 5a: mark the new node as ROOT
-      }
+        // step 5: if parent is NULL (Tree is empty)
+        if (parent = nullptr)
+        {
+            // 5a: mark the new node as ROOT 
+            ROOT = newNode;
 
+            // 5b: exit
+            return;
+        }
 
-   }
+        // step 6: if the value in the data field od new node is less than that of parent 
+        if (x < parent->info)
+        {
+            //6a: make the left of parent point to the new node
+            parent->leftchild=newNode;
+
+            //6b: exit
+            return;
+        }
+
+        // step 7: if the value in the data field of the new node is greater than that of the parent
+        else if 
+    }
 }
